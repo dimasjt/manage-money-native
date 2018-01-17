017a6b0744ef62ecf4d45f1012d5399aedea7b03
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2018-01-17 16:47:16 +0700

    AuthScreen: add facebook button

--- src/screens/AuthScreen.js
+++ src/screens/AuthScreen.js
@@ -1,7 +1,9 @@
 import React from "react"
 import { View } from "react-native"
 import { connect } from "react-redux"
-import { Button, FormInput, FormLabel } from "react-native-elements"
+import { Button, SocialIcon, FormInput, FormLabel } from "react-native-elements"
+import { Facebook } from "expo"
+// import axios from "axios"
 
 import client from "../client"
 
@@ -17,6 +19,30 @@ class AuthScreen extends React.Component {
     try {
       const result = await client.authenticate(auth)
       console.log(result)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  loginFacebook = async () => {
+    const { type, token } = await Facebook.logInWithReadPermissionsAsync("1554473021328051", {
+      permissions: ["public_profile", "email"],
+    })
+
+    try {
+      if (type === "success") {
+        // const { data } = await axios.get(`https://graph.facebook.com/me?fields=id,name,email&access_token=${token}`)
+        // const user = {
+        //   email: data.email,
+        //   password: "233bfa12d6cac8c3ae8afbc0e2aa7574e2ee2f2d",
+        //   facebookId: data.id,
+        //   strategy: "facebook",
+        // }
+
+        // const result = await client.service("users").create(user)
+        const result = await client.authenticate({ strategy: "facebook", access_token: token })
+        console.log(result)
+      }
     } catch (error) {
       console.log(error)
     }
@@ -38,6 +64,14 @@ class AuthScreen extends React.Component {
         <Button
           title="Sign In"
           onPress={this.login}
+        />
+
+        <SocialIcon
+          title="Login with Facebook"
+          button
+          type="facebook"
+          raised
+          onPress={this.loginFacebook}
         />
       </View>
     )
