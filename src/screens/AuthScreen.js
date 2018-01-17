678bc5aafe4e84069cb63313eb89e61cfb2f8f76
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2018-01-17 14:01:28 +0700

    UI: add react-native-elements

--- src/screens/AuthScreen.js
+++ src/screens/AuthScreen.js
@@ -1,12 +1,22 @@
 import React from "react"
 import { View, Text } from "react-native"
 import { connect } from "react-redux"
+import { Button } from "react-native-elements"
+
+import client from "../client"
 
 class AuthScreen extends React.Component {
+  login = () => {
+  }
+
   render() {
     return (
       <View>
         <Text>AuthScreen</Text>
+        <Button
+          title="Sign In"
+          onPress={this.login}
+        />
       </View>
     )
   }
