8a5fe23275a24ca7574bc29d11019ef33243f225
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2018-01-17 14:14:56 +0700

    feathersjs: authentication-client

--- src/client.js
+++ src/client.js
@@ -1,6 +1,8 @@
 import feathers from "@feathersjs/feathers"
 import rest from "@feathersjs/rest-client"
+import auth from "@feathersjs/authentication-client"
 import axios from "axios"
+import { AsyncStorage } from "react-native"
 
 const BASE_API = "http://192.168.0.144:3030"
 
@@ -8,5 +10,8 @@ const app = feathers()
 const restClient = rest(BASE_API)
 
 app.configure(restClient.axios(axios))
+app.configure(auth({
+  storage: AsyncStorage,
+}))
 
 export default app
