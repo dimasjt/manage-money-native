d330b6dd1f0dfc8c9ab4118b61b9475ba4f52df5
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2018-02-01 10:24:59 +0700

    add card for income and expense

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,6 +1,6 @@
 import React from "react"
-import { View, FlatList } from "react-native"
-import { Button, ButtonGroup, ListItem } from "react-native-elements"
+import { View, FlatList, StyleSheet } from "react-native"
+import { Button, ButtonGroup, ListItem, Text } from "react-native-elements"
 import { connect } from "react-redux"
 import PropTypes from "prop-types"
 
@@ -39,21 +39,53 @@ class MainScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: "#fff" }}>
-        <ButtonGroup
-          buttons={["All", "Expenses", "Incomes"]}
-          selectedIndex={this.state.filter}
-          onPress={(index) => this.setState({ filter: index })}
-        />
+        <View style={{ flex: 3, margin: 10 }}>
+          <View style={[{ flexDirection: "row" }, styles.card]}>
+            <View style={{ flex: 1 }}>
+              <Text h4>Expense</Text>
+              <Text>Rp. 10.000.000</Text>
+            </View>
+            <View style={{ flex: 1 }}>
+              <Text h4>Income</Text>
+              <Text>Rp. 20.000.000</Text>
+            </View>
+          </View>
+        </View>
 
-        <FlatList
-          data={this.props.records.data}
-          renderItem={this.renderItem}
-          keyExtractor={(item) => item.id}
-        />
+        <View style={{ flex: 2, zIndex: 10 }}>
+          <ButtonGroup
+            buttons={["All", "Expenses", "Incomes"]}
+            selectedIndex={this.state.filter}
+            onPress={(index) => this.setState({ filter: index })}
+          />
+        </View>
+
+        <View style={{ flex: 10 }}>
+          <FlatList
+            data={this.props.records.data}
+            renderItem={this.renderItem}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  card: {
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowColor: "#000000",
+    shadowRadius: 4,
+    shadowOpacity: 0.4,
+    borderRadius: 5,
+    padding: 10,
+    flex: 1,
+  },
+})
 
 MainScreen.propTypes = {
   dispatch: PropTypes.func,
