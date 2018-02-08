3feecf4633d77333cbcaa4ea3bf8ec78fa30693a
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2018-02-08 15:28:37 +0700

    show user picture and name in settings screen

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,12 +1,10 @@
 import React from "react"
 import { View } from "react-native"
-import { List, ListItem, Avatar } from "react-native-elements"
+import { List, ListItem, Avatar, Text } from "react-native-elements"
 import { connect } from "react-redux"
 import PropTypes from "prop-types"
 
 import { userLogout } from "../actions/user"
-
-import image from "../images/default_profile.png"
 
 class SettingsScreen extends React.Component {
   static navigationOptions = {
@@ -14,20 +12,23 @@ class SettingsScreen extends React.Component {
   }
 
   render() {
+    const { user, dispatch } = this.props
+
     return (
       <View style={{ flex: 1, backgroundColor: "#fff" }}>
         <View style={{ alignItems: "center", paddingTop: 24 }}>
           <Avatar
             rounded
             large
-            source={image}
+            source={{ uri: user.picture }}
           />
+          <Text h4 style={{ marginTop: 20 }}>{user.name}</Text>
         </View>
 
         <List>
           <ListItem
             title="Sign Out"
-            onPress={() => this.props.dispatch(userLogout())}
+            onPress={() => dispatch(userLogout())}
           />
         </List>
       </View>
@@ -36,7 +37,13 @@ class SettingsScreen extends React.Component {
 }
 
 SettingsScreen.propTypes = {
-  dispatch: PropTypes.func,
+  dispatch: PropTypes.func.isRequired,
+  user: PropTypes.shape({
+    picture: PropTypes.string,
+    name: PropTypes.string,
+  }).isRequired,
 }
 
-export default connect(state => state)(SettingsScreen)
+export default connect(state => ({
+  user: state.user.data,
+}))(SettingsScreen)
